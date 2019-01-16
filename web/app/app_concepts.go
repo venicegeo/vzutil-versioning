4b6ac7a8d08e7fa54fdf7baa8507c176c91300b9
wkhunt <wkhunt3@gmail.com>
2019-01-16 00:42:02 -0500

    Working on vis

--- web/app/app_concepts.go
+++ web/app/app_concepts.go
@@ -16,9 +16,17 @@ package app
 
 import (
 	"fmt"
+	"os/exec"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+)
+
+type DIR uint8
+
+const (
+	UP DIR = iota
+	DOWN
 )
 
 func (a *Application) repoConcept(c *gin.Context) {
@@ -26,10 +34,12 @@ func (a *Application) repoConcept(c *gin.Context) {
 		Sha string `form:"sha"`
 	}
 	c.Bind(&temp)
-	fmt.Println(temp)
+
 	type VNode struct {
-		Id    string `json:"id"`
-		Label string `json:"label"`
+		Id             string `json:"id"`
+		Label          string `json:"label"`
+		SecondaryLabel string `json:"labelSecondary"`
+		Level          int    `json:"level"`
 	}
 	type VEdge struct {
 		To     string `json:"to"`
@@ -38,47 +48,131 @@ func (a *Application) repoConcept(c *gin.Context) {
 	}
 	nodes := []VNode{}
 	edges := []VEdge{}
-	roots, all := generateTree(treeTestData, false)
-	for _, n := range all {
-		nodes = append(nodes, VNode{n.Sha, n.Sha})
-	}
-	for _, root := range roots {
-		traverse(root, true, func(n *Node) {
-			for _, c := range n.Children {
-				edges = append(edges, VEdge{c.Sha, n.Sha, "to"})
+	/*roots*/ _, leaves, tree := generateTree(DELETETHIS(), 0)
+	//for _, n := range tree {
+	//	nodes = append(nodes, VNode{n.Sha, n.Sha})
+	//}
+	sub := subtree(tree, leaves, 1, 3)
+	fmt.Println(leaves)
+	fmt.Println(sub)
+	fmt.Println(len(sub))
+	reset_tree(tree, 0)
+	for _, root := range leaves {
+		fmt.Println(root)
+		traverse(sub, root, 1, DOWN, func(n *Node) {
+			for _, p := range n.Parents {
+				fmt.Println("Connecting", p, "to", n.Sha)
+				edges = append(edges, VEdge{n.Sha, p, "to"})
 			}
 		})
 	}
+	reset_tree(tree, -1)
+	max := -1
+	for _, l := range leaves {
+		temp := calc_weights(sub, l, 0)
+		if temp > max {
+			max = temp
+		}
+	}
+	for _, v := range sub {
+		v.flag = max - v.flag
+	}
+	for _, n := range sub {
+		nodes = append(nodes, VNode{n.Sha, n.Sha, n.Name, n.flag})
+	}
+	//	for _, root := range roots {
+	//		traverse(tree, root, true, func(n *Node) {
+	//			for _, c := range n.Children {
+	//				edges = append(edges, VEdge{c, n.Sha, "to"})
+	//			}
+	//		})
+	//	}
 	c.HTML(200, "repo_overview_concept.html", gin.H{"nodes": nodes, "edges": edges})
 }
 
-func traverse(root *Node, setFlag bool, todo func(*Node)) {
-	if root.flag == setFlag {
+func calc_weights(tree map[string]*Node, root string, setTo int) int {
+	node := tree[root]
+	if node == nil {
+		return setTo
+	}
+	max := setTo
+	if node.flag >= setTo {
+		return max
+	}
+	node.flag = setTo
+	temp := max
+	for _, p := range node.Parents {
+		temp = calc_weights(tree, p, setTo+1)
+		if temp > max {
+			max = temp
+		}
+	}
+	return max
+}
+
+func reset_tree(tree map[string]*Node, setFlag int) {
+	for _, n := range tree {
+		n.flag = setFlag
+	}
+}
+
+func traverse(tree map[string]*Node, root string, setFlag int, direction DIR, todo func(*Node)) {
+	fmt.Println("Looking at", root)
+	if tree[root] == nil || tree[root].flag == setFlag {
+		fmt.Println(tree[root])
+		fmt.Println("returning")
 		return
 	}
-	root.flag = setFlag
-	todo(root)
-	for _, c := range root.Children {
-		traverse(c, setFlag, todo)
+	tree[root].flag = setFlag
+	todo(tree[root])
+	next := tree[root].Parents
+	if direction == UP {
+		next = tree[root].Children
+	}
+	fmt.Println("Moving to", next)
+	for _, c := range next {
+		traverse(tree, c, setFlag, direction, todo)
+	}
+}
+
+func subtree(tree map[string]*Node, roots []string, setFlag int, count int) map[string]*Node {
+	//fmt.Println("roots", roots)
+	res := make(map[string]*Node)
+	for _, r := range roots {
+		subtree_s(tree, r, setFlag, count, res)
+	}
+	return res
+}
+func subtree_s(tree map[string]*Node, root string, setFlag int, count int, res map[string]*Node) {
+	if count == 0 || tree[root].flag == setFlag {
+		//fmt.Println("Not doing", root)
+		return
+	}
+	tree[root].flag = setFlag
+	res[root] = tree[root]
+	//fmt.Println(root, tree[root].Parents)
+	for _, c := range tree[root].Parents {
+		subtree_s(tree, c, setFlag, count-1, res)
 	}
 }
 
 type Node struct {
 	Sha      string
-	Parents  []*Node
-	Children []*Node
+	Parents  []string
+	Children []string
 	Name     string
-	flag     bool
+	flag     int
 }
 
-func generateTree(data string, flag bool) ([]*Node, map[string]*Node) {
+func generateTree(data string, flag int) ([]string, []string, map[string]*Node) {
 	nodes := map[string]*Node{}
-	roots := []*Node{}
+	roots := []string{}
+	leafs := []string{}
 	lines := strings.Split(strings.TrimSpace(data), "\n")
 	parts := make([][]string, len(lines))
 	for i := 0; i < len(lines); i++ {
 		parts[i] = strings.Split(lines[i], "|")
-		nodes[parts[i][0]] = &Node{parts[i][0], []*Node{}, []*Node{}, parts[i][2], flag}
+		nodes[parts[i][0]] = &Node{parts[i][0], []string{}, []string{}, parts[i][2], flag}
 	}
 	for i := len(parts) - 1; i >= 0; i-- {
 		line := parts[i]
@@ -88,17 +182,21 @@ func generateTree(data string, flag bool) ([]*Node, map[string]*Node) {
 			if p == "" {
 				continue
 			}
-			node.Parents = append(node.Parents, nodes[p])
-			nodes[p].Children = append(nodes[p].Children, node)
+			node.Parents = append(node.Parents, p)
+			nodes[p].Children = append(nodes[p].Children, node.Sha)
 		}
 	}
-	for _, v := range nodes {
-		fmt.Println(*v)
+	for s, v := range nodes {
 		if len(v.Parents) == 0 {
-			roots = append(roots, v)
+			roots = append(roots, s)
 		}
 	}
-	return roots, nodes
+	for s, v := range nodes {
+		if len(v.Children) == 0 {
+			leafs = append(leafs, s)
+		}
+	}
+	return roots, leafs, nodes
 }
 
 var treeTestData = `
